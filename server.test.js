126c2b826df194d9ceafa3caf5838979e503fc05
Trai Torti <trai4.t@gmail.com>
2023-10-16 19:18:11 +1000

    Setup for chat app using Node.js and mongoDB

---
+++ server.test.js
@@ -0,0 +1,22 @@
+const { app, connectToDb, disconnectFromDB } = require('./server');  // Adjust the path to your server.js
+
+// Jest Setup and Teardown
+beforeAll(async () => {
+    await connectToDb();
+});
+
+afterAll(async () => {
+    await disconnectFromDB();
+});
+
+// Your actual tests would go below
+describe('Server and Database Tests', () => {
+    it('should have a running server', done => {
+        // Example test to check server
+        expect(app).toBeDefined();
+        done();
+    });
+
+    // Other tests go here...
+
+});
