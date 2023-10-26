940c6d5c2985e903b9c7fb2e704ef991bc540d8e
Trai Torti <trai4.t@gmail.com>
2023-10-26 16:47:40 +1000

    Fixed the double user tab

--- chatapp-frontend/src/views/Home.js
+++ chatapp-frontend/src/views/Home.js
@@ -1,25 +1,16 @@
 import React from 'react';
 import Header from '../components/Header';
 import ChatBox from '../components/ChatBox';
-import UserList from '../components/UserList';
 
 function Home() {
-  // This is just sample data. In a real-world scenario, you'd fetch this from your backend.
-  const sampleUsers = ['Alice', 'Bob', 'Charlie'];
+    // This is just sample data. In a real-world scenario, you'd fetch this from your backend.
 
-  return (
-    <div className="container mx-auto p-4">
-      <Header />
-      <div className="flex mt-4">
-        <div className="w-1/4 pr-4">
-          <UserList users={sampleUsers} />
+    return (
+        <div className="container mx-auto p-4">
+            <Header />
+            <ChatBox />
         </div>
-        <div className="w-3/4">
-          <ChatBox />
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Home;
