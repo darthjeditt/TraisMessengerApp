a3cc24fae74aea217830c6d9abb4aa88faa71305
Trai Torti <trai4.t@gmail.com>
2023-12-06 10:07:19 +1000

    refactored and commented controllers + updated package details.

--- chatapp-backend/controllers/chatController.js
+++ chatapp-backend/controllers/chatController.js
@@ -1,82 +1,78 @@
-const Message = require('../models/msgMdl');
-const mongoose = require('mongoose');
-const User = require('../models/userMdl');
+// chatController.js
+// This module handles the chat-related operations like fetching and posting messages.
 
-const getAllMessages = async (req, res) => {
+const MessageModel = require('../models/messageModel');
+const mongoose = require('mongoose');
+
+/**
+ * Fetches all messages from the database.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ */
+const fetchAllMessages = async (req, res) => {
     try {
-        const messages = await Message.find();
+        const messages = await MessageModel.find();
         res.json(messages);
     } catch (error) {
         console.error('Error fetching messages:', error);
-        res.status(500).send('Something went wrong!');
+        res.status(500).send('Internal Server Error.');
     }
 };
 
-const postMessage = async (req, res, io) => {
+/**
+ * Posts a new message to the database and emits it to all connected clients.
+ * @param {Object} req - The request object containing message details.
+ * @param {Object} res - The response object.
+ * @param {Object} io - The socket.io instance for real-time communication.
+ */
+const sendMessage = async (req, res, io) => {
     const { content, sender, receiver } = req.body;
+
     try {
-        const newMessage = new Message({ content, sender, receiver });
+        const newMessage = new MessageModel({ content, sender, receiver });
         await newMessage.save();
 
-        // Emit the message to all connected clients
-        io.emit('newMessage', newMessage);
+        io.emit('messageSent', newMessage); // Emit the message to all clients
 
-        return res.status(201).json({ message: 'Message sent successfully' });
+        res.status(201).json({ message: 'Message sent successfully.' });
     } catch (error) {
         console.error('Error posting message:', error);
-        return res.status(500).json({ error: 'Failed to send message' });
+        res.status(500).json({ error: 'Failed to send message.' });
     }
 };
 
-
-const getChatHistory = async (req, res) => {
+/**
+ * Retrieves the chat history between two users.
+ * @param {Object} req - The request object with user IDs.
+ * @param {Object} res - The response object.
+ */
+const fetchChatHistory = async (req, res) => {
     const { currentUserId, selectedUserId } = req.params;
 
-    // Check if both user IDs are provided
-    if (
-        !mongoose.Types.ObjectId.isValid(currentUserId) ||
-        !mongoose.Types.ObjectId.isValid(selectedUserId)
-    ) {
+    if (!mongoose.Types.ObjectId.isValid(currentUserId) ||
+        !mongoose.Types.ObjectId.isValid(selectedUserId)) {
         return res.status(400).json({
-            success: false,
-            message:
-                'Both currentUserId and selectedUserId must be valid ObjectIds.'
+            message: 'Invalid User IDs. Both must be valid ObjectIds.'
         });
     }
 
     try {
-        // Fetch messages between the two users
-        const messages = await Message.find({
+        const messages = await MessageModel.find({
             $or: [
                 { sender: currentUserId, receiver: selectedUserId },
                 { sender: selectedUserId, receiver: currentUserId }
             ]
         });
 
-        // If no messages are found, return a message indicating so
         if (!messages.length) {
-            return res.status(404).json({
-                success: false,
-                message: 'No chat history found between the two users.'
-            });
+            return res.status(404).json({ message: 'No chat history found.' });
         }
 
-        // If messages are found, return them
-        return res.status(200).json({
-            success: true,
-            data: messages
-        });
+        res.status(200).json({ data: messages });
     } catch (error) {
-        console.error('Error fetching chat history: ', error);
-        return res.status(500).json({
-            success: false,
-            message: 'Internal Server Error. Unable to fetch chat history.'
-        });
+        console.error('Error fetching chat history:', error);
+        res.status(500).json({ message: 'Internal Server Error.' });
     }
 };
 
-module.exports = {
-    getAllMessages,
-    postMessage,
-    getChatHistory
-};
+module.exports = { fetchAllMessages, sendMessage, fetchChatHistory };
--- chatapp-backend/controllers/userController.js
+++ chatapp-backend/controllers/userController.js
@@ -1,88 +1,77 @@
-const User = require('../models/userMdl');
+const UserModel = require('../models/userModel');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
+/**
+ * Registers a new user.
+ * @param {Object} req - The request object containing user details.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ */
 exports.registerUser = async (req, res, next) => {
-    const existingUser = await User.findOne({ email: req.body.email });
-
-    if (existingUser) {
-        return res.status(400).json({
-            message: 'User with this email already exists!'
-        });
-    }
-
     try {
+        const { email } = req.body;
+        const existingUser = await UserModel.findOne({ email });
 
-        const newUser = new User(req.body);
+        if (existingUser) {
+            return res.status(400).json({ message: 'Email already in use.' });
+        }
+
+        const newUser = new UserModel(req.body);
         await newUser.save();
-        console.log(`User registered: ${newUser.username}`); // <-- Added log
-        res.status(201).json({
-            message: 'User registered successfully!'
-        });
+        res.status(201).json({ message: 'User registered successfully.' });
     } catch (error) {
         next(error);
     }
 };
-exports.registerUser = async (req, res, next) => {
-    try {
-        const user = new User(req.body);
-        await user.save();
-        res.status(201).send({ message: 'User registered successfully' });
-    } catch (error) {
-        res.status(400).send({ error: error.message });
-    }
-};
 
+/**
+ * Authenticates a user and generates a JWT token.
+ * @param {Object} req - The request object containing username and password.
+ * @param {Object} res - The response object.
+ */
 exports.loginUser = async (req, res) => {
     const { username, password } = req.body;
 
     try {
-        const user = await User.findOne({ username });
-
-        console.log(`does ${password} |equal| ${user.password}`)
+        const user = await UserModel.findOne({ username });
         if (!user) {
-            console.log(`User with username: ${username} not found.`);
-            return res.status(401).json({ msg: 'Invalid credentials' });
+            return res.status(401).json({ message: 'Invalid credentials.' });
         }
+
         const isPasswordMatch = bcrypt.compareSync(password, user.password);
         if (!isPasswordMatch) {
-            console.log(`Password mismatch for username: ${username}.`);
-            return res.status(401).json({ msg: 'Invalid credentials' });
+            return res.status(401).json({ message: 'Invalid credentials.' });
         }
+
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
             expiresIn: '24h'
         });
-        console.log('Backend Generated Token:', token);
 
         res.json({ token });
-
-        if (user) {
-            console.log(`${username} has just logged in.`);
-        }
     } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server error');
+        console.error('Login error:', err.message);
+        res.status(500).send('Internal Server Error.');
     }
 };
 
+/**
+ * Retrieves the current logged-in user's details.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ */
 exports.getCurrentUser = async (req, res) => {
     try {
-        const user = await User.findById(req.user.id).select('-password');
+        const user = await UserModel.findById(req.user.id).select('-password');
         if (!user) {
-            console.error('User not found in the database');
-            return res.status(404).send({
-                message: 'User not found!'
-            });
+            return res.status(404).json({ message: 'User not found.' });
         }
-        res.send(user);
+        res.json(user);
     } catch (error) {
-        console.error('User not found in the database', error);
-        return res.status(500).send({
-            message: 'Internal Server Error'
-        });
+        console.error('Error fetching user:', error.message);
+        res.status(500).send('Internal Server Error.');
     }
 };
-
